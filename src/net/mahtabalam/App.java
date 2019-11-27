2559e3d76720a2960db104affdb6303c0beca8ef
Mahtab Alam <Mahtab.Alam@ge.com>
2019-11-27 23:21:05 +0530

    Added implementation

---
+++ src/net/mahtabalam/App.java
@@ -0,0 +1,27 @@
+package net.mahtabalam;
+
+public class App {
+
+	public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum contiguous subarray sum : "+maxSubArray(arr)); // 6
+	}
+
+	public static int maxSubArray(int[] nums) {
+		int currentMax = 0;
+		int globalMax = 0;
+		
+		if(nums == null || nums.length == 0) {
+			return Integer.MIN_VALUE;
+		} else {
+			currentMax = nums[0];
+			globalMax = nums[0];
+			for(int i = 1; i < nums.length; i++) {
+				currentMax = Math.max(currentMax + nums[i], nums[i]);
+				globalMax = Math.max(globalMax, currentMax);		
+			}
+		}
+		return globalMax;
+	}
+
+}
